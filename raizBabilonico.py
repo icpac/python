a2b901dec5617a777c1704d9ed58b977f2405974
icpac <cj.icpac@gmail.com>
2020-04-19 21:38:10 -0500

    Add files via upload

--- raizBabilonico.py
+++ raizBabilonico.py
@@ -2,13 +2,17 @@
 # javier1604@gmail.com
 #Método babilónico para encontrar una raíz cuadrada
 
-totalIteraciones = 10
-x0 = .5      # suposición inicial
-raiz = 16    # número para sacar raíz 
+totalIteraciones = 20
+x0 = 10      # suposición inicial, siempre > 0
+raiz = 17    # número al que se le sacará raíz 
 itera = 1    # 
-while itera < totalIteraciones:
+sufAprox = 0.000001 # suficiente aproximación
+
+while itera < totalIteraciones and abs(x0 - raiz / x0) > sufAprox:
     x = (1 / 2) * (x0 + raiz / x0)
-    itera += 1
     x0 = x
-    print(x)
+    itera += 1
+
+print("Cuantas iteraciones: ", itera) 
+print("Raíz de: ", raiz, "= ", x)
     
