795272cc6e5a54aea3450ce3948f46956e364acc
Tlacaelel Icpac <cj.icpac@gmail.com>
2020-09-02 15:01:55 -0500

    Add files via upload

---
+++ Covid/Covid.py
@@ -0,0 +1,48 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Covid:
+    def Datos(self):
+        #Leer archivo, separado por comas
+        nf = "200831COVID19MEXICO.csv"
+        #nf = "Covi.csv" 
+        self.df = pd.read_csv(nf, encoding = "ISO-8859-1")
+
+
+    def Miq(self):
+        print("Total de Registros : ", len(self.df.index))
+        print("Total de Defunciones: ", len(self.df.FECHA_DEF.value_counts().sort_index()[:-1]))
+        # deberían salir como 65 mil
+        sm = self.df.FECHA_DEF.value_counts().sort_index()[:-1]
+        print(sm)
+        print(sm.sum())
+        print("Último:", self.df.FECHA_DEF.value_counts().sort_index()[-1])
+
+    def YoliMiq(self):
+        s_fi = pd.Series(self.df.FECHA_INGRESO.value_counts().sort_index())
+        s_fi.plot()
+        plt.title("Fecha de Ingreso")
+        plt.show()
+        
+    def Otro(self):
+        #Obtener una serie de los datos leidos, con la columna FECHA_DEF
+        s_fd = pd.Series(df.FECHA_DEF)
+
+        #Otra serie de los datos pero, contando cuántos por FECHA_DEF
+        s_fdc = pd.Series(df.FECHA_DEF.value_counts())
+        s_fdc = pd.Series(df.FECHA_DEF.value_counts().sort_index())
+                  #[:-1])
+
+        rol_defunc = s_fdc.rolling(7)
+
+        #Imprimimos la serie
+        print(s_fd)
+        #Imprimimos la otra serie
+        print(s_fdc)
+
+
+covi = Covid()
+covi.Datos()
+covi.Miq()
+covi.YoliMiq()
