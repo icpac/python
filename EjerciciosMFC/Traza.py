34305673b9d50c5cca060a37fd8ed39cd2311a77
icpac <cj.icpac@gmail.com>
2021-10-06 06:21:46 -0500

    Traza

---
+++ EjerciciosMFC/Traza.py
@@ -0,0 +1,40 @@
+__autor__ = "Tlacaelel Icpac"
+__email__ = "tlacaelel.icpac@gmail.com"
+
+
+import numpy as np
+
+def Traza(A, B):
+    """ Multiplicación y traza """
+    AB = np.matmul(A, B)
+    print(np.matrix.trace(AB))
+
+
+""" La matriz de prueba """
+A = np.array(
+    [
+        [1, 2, -1],
+        [2, 1, 3],
+    ]
+)
+
+B = np.array(
+    [
+        [5, 4],
+        [6, 5],
+        [1, 0]
+    ]
+)
+
+Traza(A, B)
+Traza(B, A)
+
+B = np.array(
+    [
+        [2, 0],
+        [0, 2],
+    ]
+)
+
+Traza(B, B)
+
