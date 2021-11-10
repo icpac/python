7dc42a233bae8d6003e2a5af76ba2a8a425cc2e8
Javier <javier1604@gmail.com>
2021-11-09 20:51:56 -0600

    Matriz

--- EjerciciosMFC/SistEcLinGauss.py
+++ EjerciciosMFC/SistEcLinGauss.py
@@ -90,6 +90,36 @@ M = np.array(
 )
 
 
+M = np.array([
+        [.1, 2.7, 10],
+        [1.0, 0.5, -6.0]
+    ], dtype= 'f')
+
+M = np.array([
+        [1.0, 0.5, -6.0],
+        [.1, 2.7, 10]
+    ], dtype= 'f')
+
+
+M = np.array([
+        [1., -5., -1., 1.],
+        [1/6, -5/6, 1., 0.],
+        [2., -1., 0., 3.]
+    ], dtype= 'f')
+
+M = np.array([
+        [1, 4, -3, -3], 
+        [25, 97, -35, 39],
+        [35, -22, 33, -15]
+    ], dtype= 'f')
+
+"""
+M = np.array([
+        [97, 25, -35, 39],
+        [-22, 35, 33, -15],
+        [4, 1, -3, -3]
+    ], dtype= 'f')"""
+
 def SolcNon(M):
     A = M.copy
     GausNonS(M)
@@ -100,12 +130,17 @@ def Otra():
     print("Matriz aumentada: \n", M)
     Gaus = Gauss(M)
     if Gaus.all() != None:
-        print("Matriz Triangular Superior: \n", np.round(Gaus, 3))
-        Sol = Sustitucion(Gaus).reshape(3, 1)
-        print("Solucion:\n", np.round(Sol, 3))
+        #print("Matriz Triangular Superior: \n", np.round(Gaus, 3))
+        print("Matriz Triangular Superior: \n", Gaus)
+        n = M.shape[0]
+        Sol = Sustitucion(Gaus).reshape(n, 1)
+        #print("Solucion:\n", np.round(Sol, 3))
+        print("Solucion:\n", Sol)
 
         A = np.delete(A, -1, axis=1)
         print("Matriz de Coeficientes :\n", A)
-        print("Comprueba la solucion: \n", np.round(np.matmul(A, Sol), 2)) 
+        #print("Comprueba la solucion: \n", np.round(np.matmul(A, Sol), 2)) 
+        print("Comprueba la solucion: \n", np.matmul(A, Sol)) 
 
-SolcNon(M)
+#SolcNon(M)
+Otra()
