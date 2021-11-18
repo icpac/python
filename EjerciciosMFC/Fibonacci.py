1874f992a93925ceb35ee8b8bab0e28eeee682c8
Javier <javier1604@gmail.com>
2021-11-17 21:10:56 -0600

    Actualizaciones

--- EjerciciosMFC/Fibonacci.py
+++ EjerciciosMFC/Fibonacci.py
@@ -1,5 +1,11 @@
 from typing import Dict
+from functools import lru_cache
+from typing import Generator
 #https://www.manning.com/books/classic-computer-science-problems-in-python?query=Classic%20Computer%20Science%20Problems%20in%20Python#toc
+
+#Algorithms, 4th Edition, by Robert Sedgewick and Kevin Wayne (Addison-Wesley Professional, 2011)
+#https://algs4.cs.princeton.edu/home/
+
 
 def fibonacci(n):
     if n == 2:
@@ -21,6 +27,28 @@ def fibMemo(n:int)->int:
         Memo[n] = fibMemo(n-1)+fibMemo(n-2) #memoization
     return Memo[n]
         
+@lru_cache(maxsize=None)
+def fib4(n: int) -> int:  # same definition as fib2()
+    if n < 2:  # base case
+        return n
+    return fib4(n - 2) + fib4(n - 1)  # recursive case
+
+def fib5(n: int) -> int:
+    if n == 0: return n  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
+
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0  # special case
+    if n > 0: yield 1  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next  # main generation step
 
 def CualFibonacci(n):
     print(f"El enésimo número de Fibonacci: {n} \n {fibonacci(n)}")
@@ -33,3 +61,10 @@ def CualFiboMemo(n):
 
 if __name__ == "__main__":
     CualFiboMemo(17)
+    print(fib4(5))
+    print(fib4(17)) #50))
+    print(fib5(5))
+    print(fib5(50))
+
+    for i in fib6(50):
+        print(i)
