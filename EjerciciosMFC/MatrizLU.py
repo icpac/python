2e5f2f4c0926b0603d38971ca95dede1e03a3d86
Javier <javier1604@gmail.com>
2021-11-28 21:17:43 -0600

    Actualizaciones

--- EjerciciosMFC/MatrizLU.py
+++ EjerciciosMFC/MatrizLU.py
@@ -16,4 +16,33 @@ U = np.array([
     [0, 0, 0, 0]
 ])
 
+#print(np.matmul(L, U))
+
+
+P = np.array([
+[0, 1, 0 ],
+[0, 0, 1 ],
+[1, 0, 0 ]
+])
+
+A = np.array([
+[0, 0,  0, 3,  1],
+[1, 2, -3, 1, -2],
+[2, 4, -2, 1, -2] 
+])
+
+L = np.array([
+[1, 0, 0 ],
+[2, 1, 0 ],
+[0, 0, 1 ]
+])
+
+U = np.array([
+  [1, 2, -3,  1, -2],
+  [0, 0,  4, -1,  2],
+  [0, 0,  0,  3,  1]
+])
+
+print(np.matmul(P, A))
 print(np.matmul(L, U))
+#http://132.248.17.160/escuela/style/img/Redes.pdf
