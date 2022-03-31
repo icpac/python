c181e1e4638e9e3dc4480124fd8a874799d55d2e
icpac <cj.icpac@gmail.com>
2022-03-31 07:57:24 -0600

    numeros letras

--- EjerciciosMFC/Euler2.py
+++ EjerciciosMFC/Euler2.py
@@ -1,18 +1,39 @@
 import numpy as np
+import inflect
 
-np.set_printoptions(suppress=True)
-tam = 6
-path = np.zeros((tam, tam))
-for r in range(tam):
-    for c in range(tam):
-        if c > 0:
-            if r == 0:
+p = inflect.engine()
+longtud = 0
+ini = 1
+fin = 1000
+for i in range(ini, fin+1):
+    text = p.number_to_words(i)
+    longtud = longtud + len(text.replace(" ", "").replace("-", ""))
+
+print(text)
+print(longtud)
+
+
+
+
+
+def Paths():
+    np.set_printoptions(suppress=True)
+
+    from math import factorial
+    print(factorial(40)/(factorial(20))**2)
+
+    tam = 21
+    path = np.zeros((tam, tam))
+    for r in range(tam):
+        for c in range(tam):
+            if c > 0:
+                if r == 0:
+                    path[r, c] = 1
+                else:
+                    path[r, c] = path[r-1, c] + path[r, c-1]
+                    val = path[r, c]
+            elif r > 0:
                 path[r, c] = 1
-            else:
-                path[r, c] = path[r-1, c] + path[r, c-1]
-                val = path[r, c]
-        elif r > 0:
-            path[r, c] = 1
 
-print(path)
-print(val)
+    print(path)
+    print(val)
